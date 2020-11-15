72966d0e714b68d3e53b9ffd26079497ffd2815c
Diego Pereira <diegopereiraeng@gmail.com>
2020-11-15 17:12:47 -0300

    v1.1

--- CopyAppconfig.ps1
+++ CopyAppconfig.ps1
@@ -2,17 +2,36 @@ Using module './AppdynamicsConfig.psm1'
 
 $appdyConfig = [AppdynamicsConfig]::new("http://localhost:8080")
 
+#$appdyConfig.GetConfigControllers()
+
 #$appdyConfig.GetConfigControllerAppConfig | ConvertTo-Json -Depth 15
 #$appdyConfig.CopyConfigControllerAppConfig(94506170, 36 ,94506170, 49)
-$appdyConfig.GetConfigControllers()
+
+
+$srcController = 1953890731
+$destController = 1953890731
 
 #controller Original
-$apps = $appdyConfig.GetConfigApps(94506170) | Where-Object {$_.name -match "itau"} | ConvertTo-Json 
+$apps = $appdyConfig.GetConfigApps($srcController) | Where-Object {$_.name -match "itau-"}  
 
 #controller Destino
-$apps = $appdyConfig.GetConfigApps(2023422902) | Where-Object {$_.name -match "itau"} | ConvertTo-Json 
+$apps2 = $appdyConfig.GetConfigApps($destController) | Where-Object {$_.name -match "itau-"} 
+
+foreach ($app in $apps) {
+    $appName = $app.name
+    $dest = $apps2 | Where-Object {$_.name -eq $appName} 
+    if ($null -ne $dest) {
+        Write-Host "$appName Match - Copia da controller $srcController e app "$app.id" para controller $destController e app "$dest.id
+        $appdyConfig.CopyConfigControllerAppConfigBT($srcController, $app.id ,$destController, $dest.id)
+    }
+    else {
+        Write-Host "$appName Not Match - Erro na Copia da controller $srcController e app "$app.id" para controller $destController"
+    }
+}
 
 #LOGICA DE DE/PARA para chamar a copia do config
 
+
+
 # Comando de copia ( CONTROLLER_ID_ORIGEM, CONTROLLER_APP_ID_ORIGEM , CONTROLLER_ID_DESTINO , CONTROLLER_APP_ID_DESTINO )
-$appdyConfig.CopyConfigControllerAppConfigBT(94506170, 36 ,94506170, 49)
+#$appdyConfig.CopyConfigControllerAppConfigBT(94506170, 36 ,94506170, 49)
